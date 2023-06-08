6554f744fd9cdd9f1fe2cdd158b7f1222381525f
Ellertone <ellertonemain@gmail.com>
2023-06-08 22:25:17 +0300

    Importing modules

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-exe = 0
+
+exc = 0
 if number < 0:
+    exc = 1
     number *= -1
-    exe = 1
+
 lastd = number % 10
-if exe == 1:
+
+if exc == 1:
     number *= -1
     lastd *= -1
+
 print("Last digit of {:d} is ".format(number), end="")
 if lastd > 5:
     print("{:d} and is greater than 5".format(lastd))
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,1 +1,5 @@
 Python Control Statements
+
+continue -> Jumps to the top of the loop {For another iteration}
+break -> jumps out of the loop
+pass -> just a place holder when value isn't expected
